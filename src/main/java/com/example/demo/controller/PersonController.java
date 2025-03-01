0778fe745138da82117b90fcbf87846dee25fbdf
Boopathi <boopathibm19@gmail.com>
2025-03-01 19:51:15 +0530

    GET POST PUT

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Person;
@@ -19,5 +23,22 @@ public class PersonController {
     public List<Person> getAllPeople() {
         return personService.getAllPeople();
     }
-}
 
+    @GetMapping("/people/{id}")
+    public Person getPersonDetails(@PathVariable long id) {
+        var person = personService.getPerson(id);
+        return person;
+
+    }
+
+    @PostMapping("/people")
+    public Person createPersons(@RequestBody Person person){
+        return personService.createPerson(person);
+    }
+
+    @PutMapping("/people/{id}")
+    public Person updatePersons(@PathVariable long id, @RequestBody Person person) {
+        return personService.updatePerson(id, person);
+    }
+
+}
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,14 +1,17 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.Person;
 import com.example.demo.repository.PersonRepository;
 
 @Service
+@Transactional 
 public class PersonService {
 
     @Autowired
@@ -20,4 +23,30 @@ public class PersonService {
 
     }
 
+    public Person getPerson(long id) {
+        Optional<Person> newPerson = personRepository.findById(id);
+        if (newPerson.isEmpty()) {
+            throw new RuntimeException("Person not found");
+        }
+        return newPerson.get();
+    }
+
+    public Person createPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Person updatePerson(long id, Person updatePerson) {
+
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if (optionalPerson.isPresent()) {
+            var dpPerson = optionalPerson.get();
+            dpPerson.setName(updatePerson.getName());
+            dpPerson.setEmail(updatePerson.getEmail());
+
+            return personRepository.save(dpPerson);
+        }
+        throw new RuntimeException("Person not found");
+    }
+
+    
 }
